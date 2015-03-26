f9c5d42aa07b8330b351db22eefb6776706c6ca5
vidaihiep555 <letrungvi@outlook.com>
2015-03-26 07:09:30 +0700

    draw pushpin on map

---
+++ PhoneApp1/CustomPushpin/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using CustomPushpin.Resources;
+
+namespace CustomPushpin
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
